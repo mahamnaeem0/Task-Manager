94fee806148f6d1b28067790ae27db352b19bae4
Rida -e- Zahra <Rida-e-Zahra>
2023-07-17 15:19:49 +0500

    next commit

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,56 +1,240 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
-  HomeScreen(
-      {super.key,
-      required this.username,});
-  String username;
+import 'signup.dart';
+import 'login.dart';
+class Homescreen extends StatelessWidget {
+  const Homescreen ({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
+    final Size size=MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-          height: size.height,
-        width: size.width,
-        padding: const EdgeInsets.all(10),
-        color: Color.fromARGB(255, 248, 170, 26),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-    Text("Login App", style: TextStyle(color: Colors.white),),
-    ElevatedButton(
-   onPressed: () {
-                            Navigator.pop(
-                                context,
-                              );
-                          },
-  child: Icon(Icons.logout, color: Colors.white),
-  style: ElevatedButton.styleFrom(
-    shape: CircleBorder(
-       side: BorderSide(
-      color: Colors.white,
-    ),
-    ),
-    padding: EdgeInsets.all(20),
+      appBar: AppBar(backgroundColor: Colors.orange,
     
-  ),
-),
-            ],
-            
-            ),
-            SizedBox(height: 180),
-            Container(child: Text("Welcome $username", style: TextStyle(color:Colors.black),
-            ),
-            
-            ),
-            Text("Flutter",style: TextStyle(color:Colors.black, fontSize:40),),
-          ],
-        ),
-        
+      title: const Text('                       MENU CART          ',
+      style: 
+      TextStyle(fontSize: 23,fontWeight: FontWeight.bold, ),),
       ),
       
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color:Colors.grey,
+              ),
+              child: Text('   Go To   ' ,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 34),),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: const Text('login page'),
+              onTap: (){
+                Navigator.pop(context, 
+                                
+                                  
+                                    );
+              },
+            ),
+            ListTile(
+              leading: Icon(
+              Icons.car_crash,
+            ),
+            title: const Text('signup page'),
+            onTap: (){
+              Navigator.pop(context,
+                                  
+                                    );
+            },
+            ),
+
+          ],
+        ),
+      ),
+     
+    body: SingleChildScrollView(
+      padding: const EdgeInsets.all(15),
+      child: Container(
+        height: size.height,
+        width: size.width,
+        color: Colors.white,
+        padding: const EdgeInsets.only(bottom: 20),
+         child:
+             Column(
+             mainAxisAlignment: MainAxisAlignment.start,
+          
+                  children: [
+                    Padding(padding: const EdgeInsets.only(bottom: 20,),),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only( bottom: 40, left:15 ),
+                            
+                        child:
+                          
+                           InkWell(
+                            onTap:(){             
+                            }, 
+                            
+                            child:
+                             Image.asset('assets/friedrice.jpg',height: 70,width: 120,),     
+                        
+                        )
+                        ),
+                       
+                        Text('Fried Rice',style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.w100),),
+                        Column(
+                          children: [
+                            ElevatedButton(
+      onPressed: (){},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orange,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+      ),
+      child: Text(
+       "add to cart" ,
+        style: TextStyle(
+          fontSize: 12,
+          letterSpacing: 2,
+          color: Colors.white,
+        ),
+      ),
+    ),
+                            
+                            Text('price:3000',style: TextStyle(color: Colors.black,fontSize: 12,fontWeight: FontWeight.w100),),
+                          ],
+                        ),
+                        ],
+                      
+                    ),
+      
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 40, left: 15),
+                          child: InkWell(onTap:(){},
+                            child: Image.asset('assets/mancho.jpg',height: 70,width: 120,),
+
+                          
+                       
+),),
+
+Text('Mancho Soup', style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.w100),),
+Column(
+  mainAxisAlignment: MainAxisAlignment.start,
+  children:[
+          ElevatedButton(
+      onPressed: (){},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orange,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+      ),
+      child: Text(
+       "add to cart" ,
+        style: TextStyle(
+          fontSize: 12,
+          letterSpacing: 2,
+          color: Colors.white,
+        ),
+      ),
+    ),
+      Text('price:2000', style: TextStyle(fontSize: 13,fontWeight: FontWeight.w100),),
+  ],
+),
+                      ],
+                      
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 40,left: 15),
+                          child: InkWell(onTap:(){},
+                          child: Image.asset('assets/noodels.jpg',height: 70,width: 120,),
+                          ),
+                        ),
+ 
+Text('Noodles', style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.w100),),
+Column(
+  mainAxisAlignment: MainAxisAlignment.end,
+  children:[
+         ElevatedButton(
+      onPressed: (){},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orange,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+      ),
+      child: Text(
+       "add to cart" ,
+        style: TextStyle(
+          fontSize: 12,
+          letterSpacing: 2,
+          color: Colors.white,
+        ),
+      ),
+    ),
+      Text('price:2000', style: TextStyle(fontSize: 13,fontWeight: FontWeight.w100),
+      ),
+      Container(),
+  ],
+),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 40, left: 15),
+                          child: InkWell(onTap:(){},
+                          child: Image.asset('assets/wokspecial.jpg',height: 70,width: 120,),
+                          ),
+                        ),
+
+Text('Wok Special', style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.w100),),
+
+Column(
+  mainAxisAlignment: MainAxisAlignment.start,
+  children:[
+           ElevatedButton(
+      onPressed: (){},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orange,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+      ),
+      child: Text(
+       "add to cart" ,
+        style: TextStyle(
+          fontSize: 12,
+          letterSpacing: 2,
+          color: Colors.white,
+        ),
+      ),
+    ),
+      Text('price:3000', style: TextStyle(fontSize: 13,fontWeight: FontWeight.w100),),
+  ],
+),
+                      ],
+                    ),
+                  ],
+             
+           
+           ),
+           
+           ),
+    ),
     );
+    
   }
 }
